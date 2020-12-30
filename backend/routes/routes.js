abbb636384475d4ab5603f6b1b6c3c0f2b584ddf
misha <misha@bk.com>
2020-12-30 16:41:30 +0300

    create api/resister

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const {check, validationResult} = require('express-validator');
 const User = require('../models/user');
 
 
@@ -9,15 +10,30 @@ router.get('/get', (req, res) => {
     res.json("Hello!");
 })
 
-// api/add
-router.post('/add', async (req, res) => {
+// api/register
+router.post(
+    '/add',
+    [
+        check('email').isEmail(),
+        check('password').isLength({min: 5})
+    ],
+     async (req, res) => {
     try {
-        const { email, password } = req.body
+
+        const errs = validationResult(req);
+        if(!errs.isEmpty()){
+            return res.status(400).json({massage: "Error validation",errors: errs.array()})
+        }
+
+        const { email, password } = req.body;
         const condit = await User.findOne(email);
         if (condit) {
             return res.status(400).json({ massage: "A user with this email address already exists" })
         }
-        const hasPassword = await bcrypt.hash(password, "salt");
+        const hashPassword = await bcrypt.hash(password, "salt");
+        const user = new User({ email: email, password: hashPassword });
+        user.save();
+        res.status(201);
 
     } catch (e) {
         res.statusCode(500);
