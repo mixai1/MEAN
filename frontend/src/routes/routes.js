9d2b9376f074f794b250ce3080be94771b6c98c3
misha <misha@bk.com>
2020-12-31 13:12:00 +0300

    Add pages and routes

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch} from 'react-router-dom';
+
+export const Routes = (isAuth) => {
+    if(isAuth){
+        return (
+            <Switch>
+                
+            </Switch>
+        )
+    }
+}   
