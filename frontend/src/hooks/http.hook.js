214d9b1ead815ad94ba53db633f710aadce12f9a
misha <misha@bk.com>
2021-01-07 19:17:06 +0300

    create http.hook

--- frontend/src/hooks/http.hook.js
+++ frontend/src/hooks/http.hook.js
@@ -1,10 +1,28 @@
-import { useState } from "react"
-
+import { useState, useCallback } from "react"
 
 export const useHttp = () => {
     const [loading, setloading] = useState(false);
-    const req = () => {
+    const [error, setError] = useState(null);
 
-    }
-    return { loading, setloading }
+    const req = useCallback(async (url = '', method = 'GET', body = null, headers = {}) => {
+        setloading(true);
+        try {
+            const res = await fetch(url, { method, body, headers });
+            const data = await res.json();
+
+            if (!res.ok) {
+                throw new Error(data.massage);
+            }
+            setloading(false);
+
+            return data;
+        } catch (e) {
+            setloading(false);
+            setError(e.massage);
+            throw e;
+        }
+    }, []);
+    const clearError = () => { setError(null); }
+
+    return { loading, req, error, clearError }
 }
